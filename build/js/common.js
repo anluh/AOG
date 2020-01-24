292350df0b8d770f39db7bc74c3486921474f948
LFam <LFam@MacBook-Pro-LFam.local>
2020-01-24 13:19:45 +0200

    mobile fixes

--- build/js/common.js
+++ build/js/common.js
@@ -1,7 +1,21 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     $('.js-humburger').click(function() {
         $('.js-humburger').toggleClass('menu-humburger--open');
         $('.header_nav').toggleClass('header_nav--open');
     });
+
+    $('.js-nav').click(function(e) {
+      if(e.target.tagName === 'A') {
+        e.preventDefault();
+        let anchor = e.target.getAttribute('href');
+        if ($(anchor).length < 1) return
+
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top
+        }, 500);
+        if ($('.js-humburger').hasClass('menu-humburger--open')) {
+          $('.js-humburger').click();
+        }
+      }
+    })
 });
